383937d46c7f634074a9ec27c3fadf2c88ba90d1
levabala <levabala@ya.ru>
2020-02-18 08:42:39 +0300

    new specs for list

--- src/list.spec.ts
+++ src/list.spec.ts
@@ -1,4 +1,4 @@
-import { count, list } from './list';
+import { count, insert, list, listToStringWithLinks, push, shift } from './list';
 
 describe("list", () => {
   const l1 = list(1, 2, 3);
@@ -7,16 +7,72 @@ describe("list", () => {
   const l4 = list();
 
   const lists = [l1, l2, l3, l4];
-  const listsInfo: Array<{ count: number }> = [
-    { count: 3 },
-    { count: 5 },
-    { count: 2 },
-    { count: 0 }
-  ];
+
+  interface TestData {
+    args: Array<any>;
+    expected: any;
+  }
+
+  function testFunc(func: any, { args, expected }: TestData): void {
+    expect(func(...args)).toEqual(expected);
+  }
 
   it("count", () => {
-    lists.forEach((l, i) => expect(count(l)).toEqual(listsInfo[i].count));
+    const testData: TestData[] = [
+      { args: [l1], expected: 3 },
+      { args: [l2], expected: 5 },
+      { args: [l3], expected: 2 },
+      { args: [l4], expected: 0 }
+    ];
+
+    testData.forEach(data => testFunc(count, data));
   });
 
-  it("push", () => {});
+  it("push", () => {
+    const testData: TestData[] = [
+      { args: [l1, 4], expected: list(1, 2, 3, 4) },
+      {
+        args: [l2, "c"],
+        expected: list("qwe", "asd", "zxczxc", "a", "b", "c")
+      },
+      {
+        args: [l3, { c: 789 }],
+        expected: list({ a: 123 }, { b: 456 }, { c: 789 })
+      },
+      { args: [l4, null], expected: list(null) }
+    ];
+
+    testData.forEach(data => testFunc(push, data));
+  });
+
+  it("shift", () => {
+    const testData: TestData[] = [
+      { args: [l1, 4], expected: list(4, 1, 2, 3) },
+      {
+        args: [l2, "c"],
+        expected: list("c", "qwe", "asd", "zxczxc", "a", "b")
+      },
+      {
+        args: [l3, { c: 789 }],
+        expected: list({ c: 789 }, { a: 123 }, { b: 456 })
+      },
+      { args: [l4, null], expected: list(null) }
+    ];
+
+    testData.forEach(data => testFunc(shift, data));
+  });
+
+  it("insert", () => {
+    const testData: TestData[] = [
+      { args: [l1, 4, 0], expected: list(4, 1, 2, 3) },
+      { args: [l1, 4, 1], expected: list(1, 4, 2, 3) },
+      { args: [l1, 4, 2], expected: list(1, 2, 4, 3) },
+      { args: [l1, 4, 3], expected: list(1, 2, 3, 4) }
+    ];
+
+    console.log("expected:", listToStringWithLinks(list(1, 2, 3, 4)));
+    console.log("actual:", listToStringWithLinks(insert(l1, 4, 3)));
+
+    testData.forEach(data => testFunc(insert, data));
+  });
 });
--- src/list.ts
+++ src/list.ts
@@ -24,13 +24,14 @@ export function list<T>(...elements: Array<T>): List<T> {
 }
 
 export function count<T>(list: List<T>): number {
+  list = clone(list);
+
   if (!list.root) return 0;
 
-  let count = 0;
+  let count = 1;
   let currentNode = list.root;
 
-  while (currentNode) {
-    console.log(count, currentNode);
+  while (currentNode.next) {
     currentNode = currentNode.next;
     count++;
   }
@@ -40,8 +41,9 @@ export function count<T>(list: List<T>): number {
 
 export function push<T>(list: List<T>, value: T): List<T> {
   if (!list.root) return { root: { value } };
+  list = clone(list);
 
-  let lastNode = list.root;
+  let lastNode = list.root as ListElement<T>;
 
   while (lastNode.next) {
     lastNode = lastNode.next;
@@ -53,11 +55,12 @@ export function push<T>(list: List<T>, value: T): List<T> {
 
 export function shift<T>(list: List<T>, value: T): List<T> {
   if (!list.root) return { root: { value } };
+  list = clone(list);
 
   const { root } = list;
   const rootNew: ListElement<T> = { value, next: root };
 
-  root.previous = rootNew;
+  (root as ListElement<T>).previous = rootNew;
 
   return { root: rootNew };
 }
@@ -65,20 +68,28 @@ export function shift<T>(list: List<T>, value: T): List<T> {
 export function insert<T>(list: List<T>, value: T, index: number): List<T> {
   if (!list.root) return { root: { value } };
 
-  let node = list.root;
-  let i = 0;
-  while (i < index) {
-    i++;
-    node = node.next;
+  if (index === 0) return shift(list, value);
 
-    if (!node) throw errorIndexOutOfBound(i, index);
+  list = clone(list);
+
+  let node = list.root as ListElement<T>;
+  let i = 0;
+  while (++i < index) {
+    if (!node.next) throw errorIndexOutOfBound(count(list), index);
+
+    node = node.next;
   }
 
-  const newNode: ListElement<T> = { value, previous: node, next: node.next };
+  const newNode: ListElement<T> = {
+    value,
+    previous: node
+  };
 
+  if (node.next) newNode.next = node.next;
+
+  // if (node.next) node.next.previous = newNode;
   if (node.next) node.next.previous = newNode;
-
-  if (node.previous) node.previous.next = newNode;
+  node.next = newNode;
 
   return list;
 }
@@ -99,38 +110,78 @@ export function find<T>(list: List<T>, value: T): number {
   return index;
 }
 
-export function remove<T>(list: List<T>, index: number): List<T> {
-  let node = list.root;
-  if (!node) throw errorListIsEmpty();
+// export function remove<T>(list: List<T>, index: number): List<T> {
+//   let node = list.root;
+//   if (!node) throw errorListIsEmpty();
 
-  let i = 0;
-  while (node) {
-    if (i === index) {
-      node.previous.next = node.next;
-      node.next.previous = node.previous;
+//   list = clone(list);
 
-      return list;
-    }
+//   let i = 0;
+//   while (node) {
+//     if (i === index) {
+//       node.previous.next = node.next;
+//       node.next.previous = node.previous;
+
+//       return list;
+//     }
+//   }
+
+//   throw errorIndexOutOfBound(i, index);
+// }
+
+export function toArray<T>(list: List<T>): T[] {
+  const elements = [];
+  let currentNode = list.root;
+
+  while (currentNode) {
+    elements.push(currentNode.value);
+    currentNode = currentNode.next;
   }
 
-  throw errorIndexOutOfBound(i, index);
+  return elements;
 }
 
-export function removeByValue<T>(list: List<T>, value: T): List<T> {
-  let node = list.root;
-  if (!node) throw errorListIsEmpty();
+// export function removeByValue<T>(list: List<T>, value: T): List<T> {
+//   let node = list.root;
+//   if (!node) throw errorListIsEmpty();
 
-  let i = 0;
-  while (node) {
-    if (node.value === value) {
-      node.previous.next = node.next;
-      node.next.previous = node.previous;
+//   list = clone(list);
 
-      return list;
-    }
+//   while (node) {
+//     if (node.value === value) {
+//       node.previous.next = node.next;
+//       node.next.previous = node.previous;
+
+//       return list;
+//     }
+//   }
+
+//   throw errorNoSuchElement(value);
+// }
+
+export function listToString<T>(list: List<T>): string {
+  return toArray(list).join(" <-> ");
+}
+
+export function listToStringWithLinks<T>(list: List<T>): string {
+  const arr = [];
+
+  let currentNode = list.root;
+
+  while (currentNode) {
+    arr.push(
+      `${currentNode.value} (${
+        currentNode.previous ? currentNode.previous.value : "none"
+      }, ${currentNode.next ? currentNode.next.value : "none"})`
+    );
+    currentNode = currentNode.next;
   }
 
-  throw errorNoSuchElement(value);
+  return arr.join(", ");
+}
+
+export function clone<T>(listOriginal: List<T>): List<T> {
+  return list(...toArray(listOriginal));
 }
 
 export function errorIndexOutOfBound(capacity: number, index: number) {
